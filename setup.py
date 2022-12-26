8c6ea29dd4b82e1ff15628ee2558ab2c1caace9c
herbert0419 <devshobhan4@gmail.com>
2022-12-26 14:00:12 +0530

    requirements updated setup file added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+def get_requirements():
+    pass
+
+setup(
+    name="sensor",
+    version="1.0",
+    author="shobhan",
+    author_email="devshobhan4@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+
+)
